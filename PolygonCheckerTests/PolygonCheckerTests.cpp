45d09c59689285fd80b100ce1a405a6e489086b3
mtadros6762 <mtadros6762@conestogac.on.ca>
2024-03-06 13:22:19 -0500

    Add: Test classes for each function

--- PolygonCheckerTests/PolygonCheckerTests.cpp
+++ PolygonCheckerTests/PolygonCheckerTests.cpp
@@ -5,10 +5,32 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace PolygonCheckerTests
 {
-	TEST_CLASS(PolygonCheckerTests)
+
+	//Triangle Solver Test Class
+	TEST_CLASS(TriangleSolverTests)
 	{
 	public:
-		
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	//Rectangle Solver Test Class
+	TEST_CLASS(RectangleSolverTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	//Input Validation Test Class
+	TEST_CLASS(InputValidationTests)
+	{
+	public:
+
 		TEST_METHOD(TestMethod1)
 		{
 		}
