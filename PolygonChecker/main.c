e452d4fe31dc004c19f5d4d8b2a7db4489948ec5
Adil Farid <adilfarid1389@gmail.com>
2024-03-08 19:48:06 -0500

    Adding comments to the rectangle solver codes

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -28,41 +28,67 @@ int main() {
             break;
         case 2:									
             printf("Rectangle selected.\n");
-            Points* Coordinates = (Points*)malloc(RECTANGLEMAX * sizeof(Points));
-            if (Coordinates == NULL) {
-                fprintf(stderr, "Memory allocation issues\n");
-                exit(1);
-            }
-            getRectanglePoints(Coordinates);
-            
-            int Dist_AB = calculateDistance(Coordinates[0], Coordinates[1]);
-            int Dist_BC = calculateDistance(Coordinates[1], Coordinates[2]);
-            int Dist_CD = calculateDistance(Coordinates[2], Coordinates[3]);
-            int Dist_DA = calculateDistance(Coordinates[3], Coordinates[0]);
+			/*Dynamically allocating memory to store points for the rectangle and also defining Coordinates as a Points*/
+			Points* Coordinates = (Points*)malloc(MAX_POINTS * sizeof(Points));
+			if (Coordinates == NULL) {
+				fprintf(stderr, "Memory allocation issues\n");
+				exit(1);
+			}
+			/*Inputting the Coordinates defined as Points struct*/
+			getRectanglePoints(Coordinates);
 
-            float Slope_AC = calculateSlope(Coordinates[0], Coordinates[2]);
-            float Slope_BD = calculateSlope(Coordinates[1], Coordinates[3]);
+			/*We need 3 things to determine the validity of a rectangle
 
-            float angleAB = calculateAngle(Coordinates[0], Coordinates[1]);
-            float angleBC = calculateAngle(Coordinates[1], Coordinates[2]);
-            float angleCD = calculateAngle(Coordinates[2], Coordinates[3]);
-            float angleDA = calculateAngle(Coordinates[3], Coordinates[0]);
+			1: The Distance between points.
+			2: The slopes of it's diagonals.
+			3: Prove each angle is approximately equal to 90
+			********If all are true then the quadlateral is a rectangle*****
 
-            if (((Slope_AC + Slope_BD) == SLOPE_SUM) && (Dist_AB == Dist_CD) &&
-                ((int)angleAB % RIGHT_ANGLE == MIN_VALUE) && ((int)angleBC % RIGHT_ANGLE == MIN_VALUE) &&
-                ((int)angleCD % RIGHT_ANGLE == MIN_VALUE) && ((int)angleDA % RIGHT_ANGLE == MIN_VALUE)) {
-                
-                printf("\nIt's a Valid Rectangle\n");
-                int Rectangle_Perimeter = 2 * (Dist_AB + Dist_BC);
-                int Rectangle_Area = Dist_AB * Dist_BC;
-                printf("Area of Rectangle = %d\n", Rectangle_Area);
-                printf("Perimeter of Rectangle = %d \n", Rectangle_Perimeter);
-            }
-            else 
-            {
-                printf("Not a Valid Rectangle\n");
-            }
-            free(Coordinates);
+			*/
+
+			/*Finding the distance between each point using the distance formula (A to B , B to C. etc)  */
+			int Dist_AB = calculateDistance(Coordinates[0], Coordinates[1]);
+			int Dist_BC = calculateDistance(Coordinates[1], Coordinates[2]);
+			int Dist_CD = calculateDistance(Coordinates[2], Coordinates[3]);
+			int Dist_DA = calculateDistance(Coordinates[3], Coordinates[0]);
+
+			/*Finding the slopes of the diagonals*/
+			float Slope_AC = calculateSlope(Coordinates[0], Coordinates[2]);
+			float Slope_BD = calculateSlope(Coordinates[1], Coordinates[3]);
+
+			/*Finding the angles of each point*/
+			float angleAB = calculateAngle(Coordinates[0], Coordinates[1]);
+			float angleBC = calculateAngle(Coordinates[1], Coordinates[2]);
+			float angleCD = calculateAngle(Coordinates[2], Coordinates[3]);
+			float angleDA = calculateAngle(Coordinates[3], Coordinates[0]);
+
+			/*Approximating so that all the angles are in between 0 and 360 . This just makes it easier to compare the angles*/
+			if (angleAB < MIN_VALUE) angleAB += MAX_ANGLE;
+			if (angleBC < MIN_VALUE) angleBC += MAX_ANGLE;
+			if (angleCD < MIN_VALUE) angleCD += MAX_ANGLE;
+			if (angleDA < MIN_VALUE) angleDA += MAX_ANGLE;
+
+			/*Comparison of all 3 points for the validity of the rectangle*/
+			if (((Slope_AC + Slope_BD) == SLOPE_SUM) && (Dist_AB == Dist_CD) &&
+				((int)angleAB % RIGHT_ANGLE == MIN_VALUE) && ((int)angleBC % RIGHT_ANGLE == MIN_VALUE) &&
+				((int)angleCD % RIGHT_ANGLE == MIN_VALUE) && ((int)angleDA % RIGHT_ANGLE == MIN_VALUE)) {
+
+				/*Output when all are TRUE*/
+				printf("\nIt's a Valid Rectangle\n");
+				/*Finding the Perimeter of the Rectangle and Area of Rectangle*/
+				int Rectangle_Perimeter = POWER_2 * (Dist_AB + Dist_BC);
+				int Rectangle_Area = Dist_AB * Dist_BC;
+				printf("Area of Rectangle = %d\n", Rectangle_Area);
+				printf("Perimeter of Rectangle = %d \n", Rectangle_Perimeter);
+			}
+			else
+			{
+				/*Output when FALSE*/
+				printf("Not a Valid Rectangle\n");
+			}
+			/*Deallocating the memory cause honestly I have no idea but steve said it's a good practise*/
+			free(Coordinates);
+			break;
             break;
         case 0:
             continueProgram = false;
